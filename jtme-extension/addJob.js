97ed7c69148d92e49f7420f3c62f8af516558966
bbedevian <brettbedevian@gmail.com>
2020-10-29 19:47:26 -0400

    extension working finallyyyyy

--- jtme-extension/addJob.js
+++ jtme-extension/addJob.js
@@ -1,21 +1,14 @@
 /* eslint-disable no-undef */
 
-const addJob = (uid) => {
+const addJob = (uid, newJob) => {
     const firestore = firebase.firestore();
-    const company = document.getElementById("company-name-field").innerText
-    const jobTitle = document.getElementById("job-title-field").value
-    const status = document.getElementById("job-status-field").value
     let date = new Date()
     const lastContacted = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate()
-    const newJob = {company, jobTitle, status, lastContacted }
-    console.log('newJob :>> ', newJob);
+    const job = {...newJob, lastContacted }
     const collectionRef = firestore.collection('users');
     const userDoc = collectionRef.doc(uid);
     const userJobs = userDoc.collection('jobs');
-    userJobs.add(newJob);
-    document.getElementById("company-name-field").value = ""
-    document.getElementById("job-title-field").value = ""
-    document.getElementById("job-status-field").value = null
+    userJobs.add(job);
 }
 
 export default addJob
