715a8e2bbc10bf52717a73abaced67bf880c0a27
MisterRK <robert.keller22@gmail.com>
2020-11-02 21:18:40 -0500

    corrected redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -16,7 +16,7 @@ const middlewares = [sagaMiddleware];
 // if(process.env.NODE_ENV === 'development') middlewares.push(logger)
 // ???
 
-export const store = createStore(rootReducer, ...middlewares)
+export const store = createStore(rootReducer, applyMiddleware(...middlewares))
 
 sagaMiddleware.run(rootSaga)
 
