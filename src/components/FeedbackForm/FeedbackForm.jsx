9030b68960ea8b5f74f90a9353d9f44114bef59e
MisterRK <robert.keller22@gmail.com>
2020-11-17 16:55:57 -0500

    Styled feedback form

--- src/components/FeedbackForm/FeedbackForm.jsx
+++ src/components/FeedbackForm/FeedbackForm.jsx
@@ -1,37 +1,41 @@
 import React, { useState } from "react";
 import "./FeedbackForm.scss";
-import {sendFeedback} from '../../firebase/firebase.utils'
+import { sendFeedback } from "../../firebase/firebase.utils";
 
 //bootstrap imports
 import { Form, Button } from "react-bootstrap";
+import { ArrowUpCircle, ArrowDownCircle } from "react-bootstrap-icons";
 
 const FeedbackForm = () => {
 	const [feedback, setFeedback] = useState("");
+	const [display, setDisplay] = useState(false);
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		sendFeedback(feedback)
-		setFeedback("")
-	}
+		sendFeedback(feedback);
+		setFeedback("");
+	};
 	return (
 		<div className="feedback-div">
-			<Form 
-			onSubmit={handleSubmit} className="feedback-content">
-				<Form.Label>Give Us Feedback</Form.Label>
-				<Form.Control
-					name="feedback"
-					value={feedback}
-					onChange={(e) => setFeedback(e.target.value)}
-					as="textarea"
-					rows="5"
-				/>
-				<Button 
-				type="submit" 
-				className="feedback-btn" 
-				variant="success">
-					Send Feedback
-				</Button>
-			</Form>
+			<div 
+			className="feedback-header"
+			onClick={() => setDisplay(!display)}>
+				Give Us Feedback {display ? <ArrowUpCircle /> : <ArrowDownCircle />}
+			</div>
+			{display && (
+				<Form onSubmit={handleSubmit} className="feedback-content">
+					<Form.Control
+						name="feedback"
+						value={feedback}
+						onChange={(e) => setFeedback(e.target.value)}
+						as="textarea"
+						rows="5"
+					/>
+					<Button type="submit" className="feedback-btn" variant="success">
+						Send Feedback
+					</Button>
+				</Form>
+			)}
 		</div>
 	);
 };
